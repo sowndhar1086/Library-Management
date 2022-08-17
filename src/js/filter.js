98a5549ea839f4d0a541b4df62d552c1e11deeb4
Siddharth S <60936906+SiddharthSWolf@users.noreply.github.com>
2022-08-17 17:51:53 +0530

    another patch

---
+++ src/js/filter.js
@@ -0,0 +1,35 @@
+$( "#search" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+function filter(form) {
+  alert(form.bookName.value);
+}
+
+$('#txt-search').keyup(function(){
+  var searchField = $(this).val();
+if(searchField === '')  {
+$('#filter-records').html('');
+return;
+}
+
+  var regex = new RegExp(searchField, "i");
+  var output = '<div class="row">';
+  var count = 1;
+$.each(data, function(key, val){
+if ((val.employee_salary.search(regex) != -1) || (val.employee_name.search(regex) != -1)) {
+output += '<div class="col-md-6 well">';
+output += '<div class="col-md-3"><img class="img-responsive" src="'+val.profile_image+'" alt="'+ val.employee_name +'" /></div>';
+output += '<div class="col-md-7">';
+output += '<h5>' + val.employee_name + '</h5>';
+output += '<p>' + val.employee_salary + '</p>'
+output += '</div>';
+output += '</div>';
+if(count%2 == 0){
+output += '</div><div class="row">'
+}
+count++;
+}
+});
+output += '</div>';
+$('#filter-records').html(output);
+});
